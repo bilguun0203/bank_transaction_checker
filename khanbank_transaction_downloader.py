43928265fafa921cfc4e83486db4a216a6a675da
Bilguun Ochirbat <bilguun0203@gmail.com>
2021-01-28 18:31:36 +0800

    Initial commit

---
+++ khanbank_transaction_downloader.py
@@ -0,0 +1,78 @@
+import requests
+import configparser
+import argparse
+from html.parser import HTMLParser
+from datetime import date
+
+
+class BankHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        if not hasattr(self, 'extracted_data'):
+            self.extracted_data = {}
+        if tag == 'input':
+            name = ''
+            value = ''
+            for attr in attrs:
+                if attr[0] == 'name':
+                    name = attr[1]
+                elif attr[0] == 'value':
+                    value = attr[1]
+            self.extracted_data[name] = value
+
+
+class BankSession:
+    def __init__(self, login_url, download_url):
+        self.login_url = login_url
+        self.download_url = download_url
+        self.session = requests.session()
+
+    def login(self, username, password):
+        r = self.session.get(self.login_url)
+        parser = BankHTMLParser()
+        parser.feed(r.text)
+        payload = parser.extracted_data
+        payload['txtCustNo'] = username
+        payload['txtPassword'] = password
+        r = self.session.post(self.login_url, data=payload)
+
+    def isloggedin(self):
+        return '.ASPXAUTH' in self.session.cookies.keys() and '.ASPXFORMSAUTH' in self.session.cookies.keys()
+
+    def get_transactions(self, account_number, currency=None, begin_date=None, end_date=None, save_path=None):
+        if not self.isloggedin():
+            return False
+        if currency is None:
+            currency = 'MNT'
+        if end_date is None:
+            end_date = date.today().strftime('%Y.%m.%d')
+        if begin_date is None:
+            begin_date = end_date
+        r = self.session.get(self.download_url.format(
+            begin_date=begin_date, end_date=end_date, account_number=currency + 'D0000000' + account_number))
+        if save_path is not None:
+            with open(save_path, 'wb') as ofile:
+                ofile.write(r.content)
+        return r.content
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('save_path', type=str)
+    parser.add_argument('account_number', type=str)
+    parser.add_argument('currency', type=str)
+    parser.add_argument('-b', '--begin_date', type=str, default=None)
+    parser.add_argument('-e', '--end_date', type=str, default=None)
+    parser.add_argument('-c', '--config', type=str, default='config.ini')
+    args = parser.parse_args()
+
+    config = configparser.ConfigParser()
+    config.read(args.config)
+
+    bank_info = config['BANK_INFO']
+    client_info = config['CLIENT_INFO']
+
+    bank = BankSession(bank_info['LOGIN_URL'],
+                       bank_info['TRANSACTION_DOWNLOAD_URL'])
+    bank.login(client_info['USERNAME'], client_info['PASSWORD'])
+    bank.get_transactions(args.account_number, args.currency, begin_date=args.begin_date,
+                          end_date=args.end_date, save_path=args.save_path)
---
+++ looper.py
@@ -0,0 +1,62 @@
+import configparser
+from datetime import date
+from khanbank_transaction_downloader import BankSession
+import pandas as pd
+import getpass
+
+def log(bank):
+    account_number = input('Account Number: ')
+    currency = input('Currency (MNT): ')
+    today = date.today().strftime('%Y.%m.%d')
+    begin_date = input('Begin Date ({}): '.format(today))
+    end_date = input('End Date ({}): '.format(begin_date))
+    save_path = input('Save Path: ')
+    if currency == '':
+        currency = None
+    if begin_date == '':
+        begin_date = None
+    if end_date == '':
+        end_date = None
+    if save_path == '':
+        save_path = None
+    result = bank.get_transactions(account_number, currency, begin_date=begin_date, end_date=end_date, save_path=save_path)
+    if result is not False:
+        try:
+            df = pd.read_excel(result, header=1, skipfooter=1)
+            for index, row in df.iterrows():
+                print(row['Гүйлгээний огноо'], row['Эхний үлдэгдэл'], row['Дебит гүйлгээ'], row['Кредит гүйлгээ'], row['Гүйлгээний утга'])
+        except Exception as e:
+            print(e)
+    else:
+        print('Not logged in!')
+
+def login(bank):
+    username = input('Username: ')
+    password = getpass.getpass('Password: ')
+    bank.login(username, password)
+
+commands = {
+    'log': log,
+    'login': login
+}
+
+if __name__ == '__main__':
+    command = ''
+
+    config_path = input('Config Path (config.ini): ')
+    if config_path == '':
+        config_path = 'config.ini'
+    config = configparser.ConfigParser()
+    config.read(config_path)
+
+    bank_info = config['BANK_INFO']
+    client_info = config['CLIENT_INFO']
+
+    bank = BankSession(bank_info['LOGIN_URL'],
+                       bank_info['TRANSACTION_DOWNLOAD_URL'])
+    
+
+    while command != 'end':
+        command = input('> ')
+        if command in commands:
+            commands[command](bank)
